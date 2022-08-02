a62ca60e6a13b004c97f7e0d0fe87b9fb90f290f
Weed <phamhuyhoang3004@gmail.com>
2022-08-02 20:12:20 +0700

    Up ducduy's notification

--- app/src/main/java/com/example/wehelpyoubook/accountcontrol/info/ChangeNameActivity.kt
+++ app/src/main/java/com/example/wehelpyoubook/accountcontrol/info/ChangeNameActivity.kt
@@ -11,8 +11,6 @@ import com.example.wehelpyoubook.R
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class ChangeNameActivity : AppCompatActivity() {
     private var nameEdit: EditText? = null
@@ -24,51 +22,15 @@ class ChangeNameActivity : AppCompatActivity() {
         nameEdit = findViewById(R.id.name_edittext)
         save = findViewById(R.id.save_button)
         save!!.setOnClickListener {
-            val name = nameEdit!!.text.toString()
-            if (isValidateName(name)) {
-                saveDisplayName(name)
-            }
-            else {
-                Toast.makeText(this, "Cập nhật thất bại.\nVui lòng thử lại", Toast.LENGTH_SHORT).show()
-            }
+            saveDisplayName()
         }
     }
-
-    private fun isPatternName(name: String): Boolean {
-        val pattern: Pattern
-        val namePattern = "(?=.*[a-z])(?=.*[A-Z]).{4,}"
-        pattern = Pattern.compile(namePattern)
-        val matcher: Matcher = pattern.matcher(name)
-        return matcher.matches()
-    }
-    private fun isCharacterSpecial(name: String): Boolean {
-        val pattern: Pattern
-        val specialCharacters = "-@%\\[\\}+'!/#$^?:;,\\(\"\\)~`.*=&\\{>\\]<_"
-        val namePattern = "(?=.*[$specialCharacters]).{4,}"
-        pattern = Pattern.compile(namePattern)
-        val matcher: Matcher = pattern.matcher(name)
-        return matcher.matches()
-    }
-    private fun isValidateName(name: String): Boolean {
-        if (name == "") {
-            nameEdit!!.error = "Không thể để trống"
-            return false
-        }
-        if (name.length < 4) {
-            nameEdit!!.error = "Đặt đầy đủ họ tên"
-            return false
-        }
-        if (!isPatternName(name) || isCharacterSpecial(name)) {
-            nameEdit!!.error = "Tên chỉ chứa kí tự [A-Z] và [a-z]"
-            return false
-        }
-        return true
-    }
-
-    private fun saveDisplayName(name : String) {
+    private fun saveDisplayName() {
         val user = Firebase.auth.currentUser ?: return
         val profileUpdates = userProfileChangeRequest {
-            displayName = name
+            displayName = nameEdit!!.text.toString()
+            println("Tên " + nameEdit!!.text.toString())
+            println(displayName)
         }
         user.updateProfile(profileUpdates)
             .addOnCompleteListener { task ->
@@ -78,7 +40,7 @@ class ChangeNameActivity : AppCompatActivity() {
                     intent.putExtra("change", 1)
                     startActivity(intent)
                 } else {
-                    Toast.makeText(this, "Cập nhật thất bại.\nVui lòng kiểm tra internet", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Cập nhật thất bại.\nVui lòng thử lại", Toast.LENGTH_SHORT).show()
                 }
             }
     }
--- app/src/main/java/com/example/wehelpyoubook/notification/NotificationActivity.kt
+++ app/src/main/java/com/example/wehelpyoubook/notification/NotificationActivity.kt
@@ -1,83 +1,73 @@
 package com.example.wehelpyoubook.notification
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.Button
-import android.widget.RemoteViews
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.wehelpyoubook.MainActivity
 import com.example.wehelpyoubook.R
+import com.example.wehelpyoubook.accountcontrol.HomeSignInActivity
+import com.example.wehelpyoubook.databinding.ActivityMainBinding
+import com.example.wehelpyoubook.databinding.ActivityNotificationBinding
+import com.example.wehelpyoubook.databinding.ActivityUpdateDataBinding
+import com.example.wehelpyoubook.model.Orders
+import com.example.wehelpyoubook.mybooking.db
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
 
-class NotificationActivity : AppCompatActivity() {
-
-    // declaring variables
-    lateinit var notificationManager: NotificationManager
-    lateinit var notificationChannel: NotificationChannel
-    lateinit var builder: Notification.Builder
-    private val channelId = "i.apps.notifications"
-    private val description = "Test notification"
-
+class NotificationActivity() : AppCompatActivity() {
+    private val CHANNEL_ID = "1234"
+    private val notificationId = 101
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_notification)
+        //createNotificationChannel()
 
-        // accessing button
-        val btn = findViewById<Button>(R.id.btn)
+//        val btn_click_me = findViewById(R.id.btn_Notify) as Button
+//
+//        btn_click_me.setOnClickListener {
+//            sendNotify()
+//            Toast.makeText(this@NotificationActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//        }
 
-        // it is a class to notify the user of events that happen.
-        // This is how you tell the user that something has happened in the
-        // background.
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // onClick listener for the button
-        btn.setOnClickListener {
+        //R.id.btn_Notify.apply {
+            sendNotify()
+        //}
 
-            // pendingIntent is an intent for future use i.e after
-            // the notification is clicked, this intent will come into action
-            val intent = Intent(this, afterNotification::class.java)
+        //startActivity(Intent(this, MainActivity::class.java))
+    }
 
-            // FLAG_UPDATE_CURRENT specifies that if a previous
-            // PendingIntent already exists, then the current one
-            // will update it with the latest intent
-            // 0 is the request code, using it later with the
-            // same method again will get back the same pending
-            // intent for future reference
-            // intent passed here is to our afterNotification class
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    private fun sendNotify(){
+        val intent = Intent(this, NotificationActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent
+            .getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("My notification")
+            .setContentText("Booking successfully")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
-            // RemoteViews are used to use the content of
-            // some different layout apart from the current activity layout
-            val contentView = RemoteViews(packageName, R.layout.activity_after_notification)
-
-            // checking if android version is greater than oreo(API 26) or not
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
-                notificationChannel.enableLights(true)
-                notificationChannel.lightColor = Color.GREEN
-                notificationChannel.enableVibration(false)
-                notificationManager.createNotificationChannel(notificationChannel)
-
-                builder = Notification.Builder(this, channelId)
-                    .setContent(contentView)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
-                    .setContentIntent(pendingIntent)
-            } else {
-
-                builder = Notification.Builder(this)
-                    .setContent(contentView)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
-                    .setContentIntent(pendingIntent)
-            }
-            notificationManager.notify(1234, builder.build())
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, builder.build())
         }
     }
+
 }
